a60d4e85ceb72fb246c0859f63945fe1bf6adf19
Aleksei Semochkin <aleksioprime@gmail.com>
2025-07-17 15:27:17 +0300

    fix skillbar

--- frontend/app/src/components/landing/SkillsBar.vue
+++ frontend/app/src/components/landing/SkillsBar.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container mx-auto py-4">
-    <div class="mx-auto flex flex-wrap justify-center gap-3 animate-fade-in" style="max-width: 700px;">
+    <div class="mx-auto flex flex-wrap justify-center gap-3 animate-fade-in" style="max-width: 800px;">
       <span v-for="skill in skills" :key="skill"
             class="bg-neutral-800 text-cyan-200 px-4 py-2 rounded-xl text-base font-medium shadow hover:scale-105 transition cursor-default">
         {{ skill }}
