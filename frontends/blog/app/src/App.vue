589a9c70868c0deaca04299518dc396b90be84c8
Aleksei Semochkin <aleksioprime@gmail.com>
2025-10-12 15:29:48 +0300

    refactor frontends

---
+++ frontends/blog/app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Главная</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
